42716d3673ea84ae0dc630672467314dab2dba8d
Matt Rice <mattrice@Matts-MacBook-Pro.local>
2019-01-14 17:44:23 -0600

    updated gem to permit different credentials

--- lib/google-oauth2-installed.rb
+++ lib/google-oauth2-installed.rb
@@ -6,13 +6,13 @@ require 'google-oauth2-installed/access_token'
 module GoogleOauth2Installed
 
   # A centralized place to access all loaded configuration and defaults.
-  def self.credentials
+  def self.credentials(user_account="")
     {
       method: 'OAuth2',
-      oauth2_client_id: ENV["OAUTH2_CLIENT_ID"],
-      oauth2_client_secret: ENV["OAUTH2_CLIENT_SECRET"],
-      oauth2_token: oauth2_token,
-      oauth2_scope: ENV['OAUTH2_SCOPE'],
+      oauth2_client_id: oauth2_client_id(user_account),
+      oauth2_client_secret: oauth2_client_secret(user_account),
+      oauth2_token: oauth2_token(user_account),
+      oauth2_scope: oauth2_scope(user_account),
       oauth2_redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
       oauth2_urls: {
         authorize_url: 'https://accounts.google.com/o/oauth2/auth',
@@ -21,8 +21,10 @@ module GoogleOauth2Installed
     }
   end
 
-  def self.access_token
-    AccessToken.new(credentials).access_token
+  def self.access_token(user_account="")
+    user_account = "_#{user_account}" unless user_account.blank?
+    creds = credentials(user_account)
+    AccessToken.new(creds).access_token
   end
 
   # To be used interactively
@@ -32,12 +34,24 @@ module GoogleOauth2Installed
 
   private
 
-  def self.oauth2_token
-    if ENV["OAUTH2_ACCESS_TOKEN"]
+  def oauth2_client_id(user_account)
+    ENV["OAUTH2_CLIENT_ID#{user_account}"]
+  end
+
+  def oauth2_client_secret(user_account)
+    ENV["OAUTH2_CLIENT_SECRET#{user_account}"]
+  end
+
+  def oauth2_scope(user_account)
+    ENV["OAUTH2_SCOPE#{user_account}"]
+  end
+
+  def self.oauth2_token(user_account)
+    if ENV["OAUTH2_ACCESS_TOKEN#{user_account}"]
       {
-        access_token: ENV["OAUTH2_ACCESS_TOKEN"],
-        refresh_token: ENV["OAUTH2_REFRESH_TOKEN"],
-        expires_at: ENV["OAUTH2_EXPIRES_AT"].to_i,
+        access_token: ENV["OAUTH2_ACCESS_TOKEN#{user_account}"],
+        refresh_token: ENV["OAUTH2_REFRESH_TOKEN#{user_account}"],
+        expires_at: ENV["OAUTH2_EXPIRES_AT#{user_account}"].to_i,
       }
     end
   end
